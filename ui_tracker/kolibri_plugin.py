30160c44a2af1ea6a871397253dd3640cf901d11
66eli77 <66eli77@gmail.com>
2016-10-01 23:54:00 -0700

    logic for adding the ui_tracking JS module into kolibri base.html template also for setting the ui_tracking API url -- ui_tracker.png/

--- ui_tracker/kolibri_plugin.py
+++ ui_tracker/kolibri_plugin.py
@@ -3,19 +3,22 @@ from __future__ import absolute_import, print_function, unicode_literals
 from kolibri.core.webpack import hooks as webpack_hooks
 from kolibri.plugins.base import KolibriPluginBase
 
-from . import hooks, urls
+from . import urls
 
 
 class UITrackerPlugin(KolibriPluginBase):
-    pass
+    def url_module(self):
+        return urls
+
+    def url_slug(self):
+        return "^ui_tracker.png"
 
 
 class UITrackerAsset(webpack_hooks.WebpackBundleHook):
     unique_slug = "ui_tracker_module"
     src_file = "assets/src/module.js"
-    events = {
-        "content_render:exercise/perseus": "render"
-    }
 
-class UITrackerInclusionHook(hooks.UITrackerSyncHook):
+
+
+class UITrackerInclusionHook(webpack_hooks.FrontEndBaseSyncHook):
     bundle_class = UITrackerAsset
