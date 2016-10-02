74ceaa65ab245d2befaa9d1ad29d37e246b952fc
66eli77 <66eli77@gmail.com>
2016-10-01 23:55:42 -0700

    basic client side JS module for tracking click event, more event tracking will go here soon this module will be loaded into the kolibri base.html, therefore in every kolibri page.

--- ui_tracker/assets/src/module.js
+++ ui_tracker/assets/src/module.js
@@ -1,31 +1,49 @@
 const KolibriModule = require('kolibri_module');
-const ExerciseComponent = require('./vue/index');
 
 class UITrackerModule extends KolibriModule {
 
-  get rendererComponent() {
-    return ExerciseComponent;
+  // gather basic info.
+  basicInfo() {
+    return {
+      time: Date.now(),
+      facilityID: window.kolibriGlobal.coreVue.vuex.store.initialState.core.session.facility_id,
+      userID: window.kolibriGlobal.coreVue.vuex.store.initialState.core.session.user_id,
+      userName: window.kolibriGlobal.coreVue.vuex.store.initialState.core.session.username,
+      userKind: window.kolibriGlobal.coreVue.vuex.store.initialState.core.session.kind,
+    };
   }
 
-  // gather data somehow
-  // data() {
-  //   return {
-  //     time: Date.now(),
-  //     pixelRatio: window.devicePixelRatio
-  //   };
-  // }
+  // construct query string.
+  query(data) { 
+    return Object.keys(data).map(function(key){
+      return encodeURIComponent(key) + '=' + encodeURIComponent(data[key]);
+    }).join('&');
+  }
 
-  // // construct query string
-  // query(data) { 
-  //   return Object.keys(data).map(function(key){
-  //     return encodeURIComponent(key) + '=' + encodeURIComponent(data[key]);
-  //   }).join('&');
-  // }
+  // send back click tracking data. 
+  clickQuery(e) {
+    const data = this.basicInfo();
+    data['targetClass'] = e['target'].className;
+    data['targetID'] = e['target'].id;
+    data['type'] = 'click';
+    console.log('dddd:::: ', data);
+    new Image().src = '/ui_tracker.png?' + this.query(data);
+  }
 
-  // ready() {
-  //   console.log('elielieli');
-  //   new Image().src = '/uitrk.png?' + this.query(this.data()); // "/stats.png?time=1391976334544&pixelRatio=1"
-  // }
+  // inherited callback when this module is initialized.
+  initialize() {
+    console.log('inininininin:::: ');
+  }
+
+  // inherited callback when this module is loaded.
+  ready() {
+    const clickQuery = this.clickQuery;
+    const that = this;
+    document.onclick = function(e) {
+      window.eli = e;
+      clickQuery.apply(that, [e]);
+    };
+  }
 }
 
 module.exports = new UITrackerModule();
